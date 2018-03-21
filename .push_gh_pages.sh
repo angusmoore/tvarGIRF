5a8f4609312c655289b0a6c346c499d0ae905e2b
angusmoore <18376371+angusmoore@users.noreply.github.com>
2018-03-21 17:22:52 +1100

    Fix up push_gh_pages

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -1,4 +1,4 @@
-R -e "path <- '../tvarGIRF'; system(paste(shQuote(file.path(R.home('bin'), 'R')), 'CMD', 'Rd2pdf', shQuote(path)))"
+#!/bin/bash
 
 rm -rf out || exit 0;
 mkdir out;
@@ -11,12 +11,15 @@ for files in '*.tar.gz'; do
         tar xfz $files
 done
 
+R -e "path <- '../tvarGIRF'; system(paste(shQuote(file.path(R.home('bin'), 'R')), 'CMD', 'Rd2pdf', shQuote(path)))"
+
 cd out
 git init
 git config user.name "travis"
 git config user.email "travis"
+
 cp ../tvarGIRF.pdf tvarGIRF.pdf
 
 git add .
-git commit -m "Rebuilt PDF package documentation and deployed to gh-pages"
+git commit -m "Auto-deploy docs to github pages"
 git push --force --quiet $FULL_REPO master:gh-pages
