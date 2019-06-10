39c7ec8cfbff2d31a30db8b44291a4f829122a05
Angus <18376371+angusmoore@users.noreply.github.com>
2019-06-10 17:50:21 +1000

    Compare tests against known result by hash. Test error if pass in incorrect shock.

--- tests/testthat/test-GIRF.R
+++ tests/testthat/test-GIRF.R
@@ -1,7 +1,18 @@
 library(tsDyn)
 data(zeroyld)
-tv <- TVAR(zeroyld, lag=2, nthresh=1, thDelay=1, trim=0.1, mTh=1, plot=FALSE)
-expect_error(GIRF(tv, c(0,1), horizon = 10, R = 2, H = 2), NA)
+set.seed(42)
 
-tv <- TVAR(zeroyld, lag=2, nthresh=2, thDelay=1, trim=0.1, mTh=1, plot=FALSE)
-expect_error(GIRF(tv, c(0,1), horizon = 10, R = 2, H = 2), NA)
+test_that("Known output tests", {
+  tv <- TVAR(zeroyld, lag=2, nthresh=1, thDelay=1, trim=0.1, mTh=1, plot=FALSE)
+  expect_known_hash(GIRF(tv, c(0,1), horizon = 10, R = 2, H = 2), "bb3f")
+
+  tv <- TVAR(zeroyld, lag=2, nthresh=2, thDelay=1, trim=0.1, mTh=1, plot=FALSE)
+  expect_known_hash(GIRF(tv, c(0,1), horizon = 10, R = 2, H = 2), "5937")
+})
+
+
+test_that("Errors for bad input", {
+  tv <- TVAR(zeroyld, lag=2, nthresh=1, thDelay=1, trim=0.1, mTh=1, plot=FALSE)
+  expect_error(GIRF(tv, 2, horizon = 10, R = 2, H = 2),
+               "Your shock vector has the wrong length")
+})
